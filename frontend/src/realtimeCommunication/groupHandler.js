e2c5ad563689964fee372160a07a851e849c208c
Vikas <provikas2001@gmail.com>
2023-04-12 15:02:28 +0530

    added code for removing the user from group member list when user left the group

--- frontend/src/realtimeCommunication/groupHandler.js
+++ frontend/src/realtimeCommunication/groupHandler.js
@@ -1,5 +1,6 @@
 import { openAlert } from "../store/actions/alertAction";
 import {
+  chatActions,
   chatTypes,
   setChosenChatDetails,
   setCurrentConversation,
@@ -18,25 +19,26 @@ export const groupUpdate = (data) => {
     const newGroupPopulated = groupUpdate.find((group) => {
       return newGroup._id === group._id;
     });
-    if (store.getState().auth.userDetail.id === newGroupPopulated.admin) {
+    if (store.getState().auth.userDetail.id === newGroupPopulated.admin._id) {
       severity = "success";
       content = `'${newGroupPopulated.name}' group successfully created`;
+      chosenChatDetails = {
+        groupId: newGroupPopulated._id,
+        username: newGroupPopulated.name,
+        admin: newGroupPopulated.admin,
+        date: newGroupPopulated.date,
+        members: newGroupPopulated.conversation.participants,
+      };
+      currentConversationId = newGroupPopulated.conversation._id;
+      store.dispatch(setChosenChatDetails(chosenChatDetails, chatTypes.GROUP));
+      store.dispatch(setCurrentConversation(currentConversationId));
     } else {
       content = `You were added to '${newGroupPopulated.name}' group `;
     }
     store.dispatch(openAlert(content, severity));
-    chosenChatDetails = {
-      groupId: newGroupPopulated._id,
-      username: newGroupPopulated.name,
-      admin: newGroupPopulated.admin,
-      date: newGroupPopulated.date,
-      members: newGroupPopulated.conversation.participants,
-    };
-    currentConversationId = newGroupPopulated.conversation._id;
-    store.dispatch(setChosenChatDetails(chosenChatDetails, chatTypes.GROUP));
-    store.dispatch(setCurrentConversation(currentConversationId));
   } else if (deletedGroup) {
     if (store.getState().auth.userDetail.id === deletedGroup.admin) {
+      severity = "success";
       content = `'${deletedGroup.name}' group successfully deleted`;
     } else {
       content = `'${deletedGroup.name}' group deleted`;
@@ -44,7 +46,27 @@ export const groupUpdate = (data) => {
     store.dispatch(openAlert(content, severity));
     store.dispatch(setChosenChatDetails(chosenChatDetails));
     store.dispatch(setCurrentConversation(currentConversationId));
+  } else {
   }
 
   store.dispatch(setGroups(groupUpdate));
+  //If current group is open then the left user should be removed from group member list which is present in chosenChatDetails.members. we need to update that
+  const currentGroup = groupUpdate.find((group) => {
+    if (store.getState().chat.conversationId === group.conversation._id)
+      return group;
+  });
+  if (currentGroup) {
+    const chosenChatDetails = store.getState().chat.chosenChatDetails;
+    console.log(chosenChatDetails);
+    store.dispatch(
+      setChosenChatDetails(
+        {
+          ...chosenChatDetails,
+          members: currentGroup.conversation.participants,
+        },
+        chatActions.GROUP
+      )
+    );
+  }
+  console.log(currentGroup);
 };
