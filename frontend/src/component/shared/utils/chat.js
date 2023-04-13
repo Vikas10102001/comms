f7009d54cedcae1645ad0db9ac370e4feafc7831
Vikas <provikas2001@gmail.com>
2023-04-13 23:39:23 +0530

    update chats for chat type direct on the basis of comparing participants and for type group compare conversationId

--- frontend/src/component/shared/utils/chat.js
+++ frontend/src/component/shared/utils/chat.js
@@ -1,12 +1,38 @@
 import store from "../../../store/store";
 import { setMessages } from "../../../store/actions/chatAction";
+import { getCurrentConversation } from "../../../realtimeCommunication/connectSocketServer";
 
 export const updateChatHistory = (data) => {
-  const { conversationId, messages } = data;
-  
-  const currentConversationId = store.getState().chat.conversationId;
-  const checkConversation = conversationId === currentConversationId ;
-  if (checkConversation) {
-    store.dispatch(setMessages(messages));
+  const { participants, messages, type, conversationId } = data;
+  const chatType = store.getState().chat.chatType;
+  console.log(participants);
+  // if (store.getState().chat.chatType === "DIRECT") {
+  //   const id = store.getState().chat.chosenChatDetails.id;
+  //   getCurrentConversation({ recieverId: id });
+  // }
+  let chosenCoversationPartipants = [];
+  let checkConversation;
+  if (chatType === type) {
+    if (chatType === "DIRECT") {
+      const userId = store.getState().auth.userDetail.id;
+      const recieverId = store.getState().chat.chosenChatDetails.id;
+      chosenCoversationPartipants = [userId, recieverId];
+      checkConversation =
+        participants.length === chosenCoversationPartipants.length
+          ? participants.every((element) => {
+              if (chosenCoversationPartipants.includes(element)) {
+                return true;
+              }
+              return false;
+            })
+          : false;
+    }
+    if (chatType === "GROUP") {
+      checkConversation =
+        store.getState().chat.conversationId === conversationId;
+    }
+    if (checkConversation) {
+      store.dispatch(setMessages(messages));
+    }
   }
 };
