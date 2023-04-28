f601ab640d9fcb7e344928f632d9de3660572aae
Vikas <provikas2001@gmail.com>
2023-04-28 22:22:26 +0530

    showing spinner based on loading state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Dashboard from "./component/Dashboard/Dashboard";
 import Register from "./component/auth/Register/Register";
 import "./App.css";
 import AlertNotification from "./component/shared/component/AlertNotification";
+import Spinner from "./component/shared/component/Spinner";
+import { useSelector } from "react-redux";
 
 function App() {
   // useEffect(() => {
@@ -15,6 +17,10 @@ function App() {
   //     },
   //   };
   // }, []);
+  const isLoading = useSelector((state) => {
+    return state.spinner.isLoading;
+  });
+  console.log(isLoading);
   return (
     <>
       <Routes>
@@ -24,6 +30,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <AlertNotification />
+      {isLoading && <Spinner />}
     </>
   );
 }
